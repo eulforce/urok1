bedd238f35a4a79fd821080cfd9193010d674dee
eulforce <eulforce@gmail.com>
2024-05-13 03:37:38 +0600

    urok1 zadanie3 yura-gagarin

---
+++ src/main/kotlin/urok1_zadanie1/urok1_zadanie3.kt
@@ -0,0 +1,19 @@
+package org.example.urok1_zadanie1
+
+import java.sql.Time
+
+fun main(){
+    var year: Int = 1961
+    //val hour: Int = 9
+    //val minute: Int = 7
+    //println(year)
+    //println("%02d".format(hour))
+    //println("%02d".format(minute))
+    val hour: Int = 10
+    val minute: Int = 55
+    println("$hour:$minute")
+
+
+
+
+}
