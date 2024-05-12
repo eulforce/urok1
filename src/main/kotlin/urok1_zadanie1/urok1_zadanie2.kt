78c720a7506250b1505506674fce94a38358a1ff
eulforce <eulforce@gmail.com>
2024-05-13 01:25:24 +0600

    urok1 samozadanie2 uvolilsya

---
+++ src/main/kotlin/urok1_zadanie1/urok1_zadanie2.kt
@@ -0,0 +1,18 @@
+
+
+fun main() {
+    val numberOfOrder: Int = 75
+    val thanksForPurchase: String = "Спасибо за покупку, приходите к нам еще!"
+    println(numberOfOrder)
+    println(thanksForPurchase)
+    //val amountOfWorkers: Int = 2000
+
+    //println(amountOfWorkers)
+    val workerInShop: Int = 2000
+    val resignedEmployee: Int = 1
+    println(workerInShop - resignedEmployee)
+
+
+
+}
+
