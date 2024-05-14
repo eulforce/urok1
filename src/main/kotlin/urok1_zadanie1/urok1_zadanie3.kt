b409749bcd4ea6247e9117c4bfcd7c4858a590ba
eulforce <eulforce@gmail.com>
2024-05-14 21:51:04 +0600

    urok1 zadanie3 peredelka

--- src/main/kotlin/urok1_zadanie1/urok1_zadanie3.kt
+++ src/main/kotlin/urok1_zadanie1/urok1_zadanie3.kt
@@ -3,15 +3,25 @@ package org.example.urok1_zadanie1
 import java.sql.Time
 
 fun main(){
-    var year: Int = 1961
-    //val hour: Int = 9
-    //val minute: Int = 7
+    val year: Int = 1961
+    var hour = "09"
+    //комментировать это не надо, а вывести, и потом эту переменную заменить на новые данные.
+    var minute = "07"
+
     //println(year)
+    //println(hour)
+    //println(minute)
     //println("%02d".format(hour))
+    // Хорошо что знаешь про использования format для вывод 2 знаков.
+    // Вообще нормальная практика, если сервер присылает дату или время строкой.
+    // Давай объявим строки и сделаем правильный формат)
     //println("%02d".format(minute))
-    val hour: Int = 10
-    val minute: Int = 55
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
+    //print(hour)
+    //print(minute)
+
 
 
 
